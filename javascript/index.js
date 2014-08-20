fc472b8700fa3baba8abce3531cf37f3a88ffb6d
Matt Rocco <roccoma@rose-hulman.edu>
2014-08-20 12:58:18 -0400

    test

--- javascript/index.js
+++ javascript/index.js
@@ -9,11 +9,18 @@ $('#aboutUsTab a').click(function (e) {
   e.preventDefault()
   $(this).tab('show');
 });
+$('#brothersTab a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show');
+});
 $("#tab1").tab('show');
 $('.carousel').carousel();
 
 $("#tab2").tab('show');
 $("#tab3").tab('show');
 
+$('#sophmoresCarousel').carousel();
+$('#juniorsCarousel').carousel();
+$('#seniorCarousel').carousel();
 
 });
