598531fa740007640dfdd7b92f879016266f098d
Matt Rocco <roccoma@rose-hulman.edu>
2014-08-20 16:13:18 -0400

    test

--- javascript/index.js
+++ javascript/index.js
@@ -23,4 +23,40 @@ $('#sophmoresCarousel').carousel();
 $('#juniorsCarousel').carousel();
 $('#seniorCarousel').carousel();
 
+
+$('#calendar1').fullCalendar({
+			header: {
+				center: 'September Rush',
+				right: 'month,agendaWeek,agendaDay'
+			},
+			defaultDate: '2014-09-01',
+			selectable: true,
+			selectHelper: true,
+			select: function(start, end) {
+				var title = prompt('Event Title:');
+				var eventData;
+				if (title) {
+					eventData = {
+						title: title,
+						start: start,
+						end: end
+					};
+					$('#calendar').fullCalendar('renderEvent', eventData, true); // stick? = true
+				}
+				$('#calendar').fullCalendar('unselect');
+			},
+			events: [
+				{
+					title: 'Greek Fair',
+					start: '2014-09-04'
+				},
+				{
+					title: 'Dorm Storming',
+					start: '2014-09-15T7:00:00',
+					end: '2014-09-10T10:00:00'
+				},
+
+			]
+		});
+
 });
